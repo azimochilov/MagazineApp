c2405d0aec2d1c6af11d93e6a742f187f92a9433
azimochilov <azimochilov226@gmail.com>
2023-09-14 16:55:21 +0500

    first commit

---
+++ MagazineApp/Domain/Commons/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MagazineApp.Domain.Commons;
+public abstract class Auditable
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    public long? UpdatedBy { get; set; }
+    public long? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ MagazineApp/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using MagazineApp.Domain.Commons;
+using System.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagazineApp.Domain.Entities;
+public class User : Auditable
+{
+    [MinLength(3), MaxLength(50)]
+    public string FirstName { get; set; }
+    [MinLength(3), MaxLength(50)]
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
