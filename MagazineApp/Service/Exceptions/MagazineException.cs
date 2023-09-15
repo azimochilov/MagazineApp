57a4e81b8378b3d9c8e02c64172e3675458796b8
azimochilov <azimochilov226@gmail.com>
2023-09-15 18:16:00 +0500

    exception is added

---
+++ MagazineApp/Service/Exceptions/MagazineException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MagazineApp.Service.Exceptions;
+public class MagazineException : Exception
+{
+    public int Code { get; set; }
+    public MagazineException(int code = 500, string message = "Something went wrong") : base(message)
+    {
+        this.Code = code;
+    }
+}
